d232c7d711d0b8a117b494e5787ba11598be6821
arson5012 <74953111+arson5012@users.noreply.github.com>
2024-06-12 17:25:19 +0900

    Update current image save when RBClick
    마우스 우 클릭 시 현재(확대/축소)의 이미지를 현 파일 디렉터리에 현재 시간 이름으로 저장

---
+++ cvMouseExtension.cpp
@@ -0,0 +1,267 @@
+#include "pch.h"
+#include "cvMouseExtension.h"
+
+// 마우스 이벤트 (확대/축소: 휠, 이동: 마우스 좌클릭 - 드래그, 우클릭: 줌 상태의 이미지 저장)
+void cvMouseCallBack (int event, int x, int y, int flag, void* pUserData)
+{
+	cvMouseExtension* pParent = (cvMouseExtension*)pUserData;
+	
+	if (event == EVENT_MOUSEWHEEL) 
+	{
+		if (getMouseWheelDelta(flag) > 0 && pParent->zoomCount != pParent->maxZoomCount)
+			pParent->zoomCount++;
+		else if (getMouseWheelDelta(flag) < 0 && pParent->zoomCount != pParent->minZoomCount)
+			pParent->zoomCount--;
+
+		if (pParent->zoomCount == 0)
+			pParent->offsetX = pParent->offsetY = 0;
+
+		x = pParent->ptLButtonDown.x;
+		y = pParent->ptLButtonDown.y;
+
+		// double형으로 캐스팅하여 오버플로 방지
+		double horzSliderBarPos = static_cast<double>(pParent->horizontalSliderPos);
+		double vertSliderBarPos = static_cast<double>(pParent->verticalSliderPos);
+
+		double dPixelX = (horzSliderBarPos + x + pParent->offsetX) / pParent->newScale;
+		double dPixelY = (vertSliderBarPos + y + pParent->offsetY) / pParent->newScale;
+
+		pParent->newScale = pParent->InitScale * pow(pParent->scaleRatio, pParent->zoomCount);
+
+		int iW = pParent->originalWidth;
+		int iH = pParent->originalHeight;
+		pParent->horizontalSliderRangeMax = int(pParent->newScale * iW - pParent->InitScale * iW);
+		pParent->verticalSliderRangeMax = int(pParent->newScale * iH - pParent->InitScale * iH);
+
+		// double형으로 캐스팅하여 오버플로 방지
+		int iBarPosX = static_cast<int>(dPixelX * pParent->newScale - x + 0.5);
+		int iBarPosY = static_cast<int>(dPixelY * pParent->newScale - y + 0.5);
+
+		pParent->SetHorzSliderCtrlPos(iBarPosX);
+		pParent->SetVertSliderCtrlPos(iBarPosY);
+
+		// double형으로 캐스팅하여 오버플로 방지
+		pParent->offsetX = -iBarPosX + (dPixelX * pParent->newScale - x);
+		pParent->offsetY = -iBarPosY + (dPixelY * pParent->newScale - y);
+
+		pParent->InvalidateWindows();
+	}
+	else if (event == EVENT_LBUTTONDOWN)
+	{
+		pParent->ptLButtonDown.x = x;
+		pParent->ptLButtonDown.y = y;
+	}
+	else if (flag == EVENT_FLAG_LBUTTON)
+	{
+		int iRButtonOffsetX = x - pParent->ptLButtonDown.x;
+		int iRButtonOffsetY = y - pParent->ptLButtonDown.y;
+
+
+		int iBarPosX = pParent->horizontalSliderPosCopy - iRButtonOffsetX;
+		pParent->SetHorzSliderCtrlPos (iBarPosX);
+		
+
+		int iBarPosY = pParent->verticalSliderPosCopy - iRButtonOffsetY;
+		pParent->SetVertSliderCtrlPos (iBarPosY);
+		pParent->InvalidateWindows();
+	}
+
+	else if (event == EVENT_RBUTTONDOWN)
+	{
+		pParent->OnRButtonSaveImage();
+	}
+	else if (event == EVENT_MOUSEMOVE)
+	{
+		pParent->ptLButtonDown.x = x;
+		pParent->ptLButtonDown.y = y;
+		pParent->horizontalSliderPosCopy = pParent->horizontalSliderPos;
+		pParent->verticalSliderPosCopy = pParent->verticalSliderPos;
+
+	}
+}
+
+// 초기화
+cvMouseExtension::cvMouseExtension(String strWindowName, int iFlag)
+{
+	namedWindow (strWindowName, iFlag);
+	resizeWindow(strWindowName, cv::Size(800, 800)); // 초기 윈 사이즈 설정
+	setMouseCallback (strWindowName, cvMouseCallBack, this);
+	m_strWindowName = strWindowName;
+
+
+	//Initial values
+	zoomCount = 0;
+	maxZoomCount = 10; // 줌 카운트 (x10)
+	minZoomCount = 0;
+
+	m_Resize.resize (maxZoomCount + 1);
+
+	offsetX = 0;
+	offsetY = 0;
+	InitScale = 1;
+	newScale = 1;
+	scaleRatio = 1.25; // 줌 스케일
+
+	originalWidth = 0;
+	originalHeight = 0;
+
+	horizontalSliderPos = 0;
+	verticalSliderPos = 0;
+
+	horizontalSliderRangeMin = 0;
+	horizontalSliderRangeMax = 1;
+	verticalSliderRangeMin = 0;
+	verticalSliderRangeMax = 1;
+
+}
+cvMouseExtension::~cvMouseExtension()
+{
+	if (!m_strWindowName.empty() && windowExists(m_strWindowName)) 
+	{
+		setMouseCallback(m_strWindowName, nullptr);
+	}
+
+
+	if (!Image.empty())
+		Image.release();
+	for (auto& img : m_Resize) 
+	{
+		if (!img.empty())
+			img.release();
+	}
+	m_Resize.clear();
+
+	cv::destroyAllWindows();
+}
+bool cvMouseExtension::windowExists(const string& windowName)
+{
+	Mat testImage; // 임시 이미지 생성
+	try 
+	{
+		imshow(windowName, testImage); // 윈도우를 표시해보고
+		if (getWindowProperty(windowName, WND_PROP_AUTOSIZE) != -1) 
+		{
+			cv::destroyAllWindows(); // 윈도우가 존재하면 제거
+			return true;
+		}
+	}
+	catch (const cv::Exception&) {}
+	return false; // 윈도우가 존재하지 않거나 예외가 발생한 경우
+}
+
+// ImRead: 파일 불러오기 / ImShow: Mat 불러오기
+bool cvMouseExtension::ImRead(String winname, double InitScale)
+{
+	SetInitailScale(InitScale);
+
+	Image = imread (winname);
+
+	if (Image.empty())
+		return false;
+	originalWidth = Image.cols;
+	originalHeight = Image.rows;
+
+	Size sizeInitial(Image.cols * InitScale, Image.rows * InitScale);
+	resize(Image, m_Resize[0], sizeInitial, 0, 0, resizeFlag);
+	if (!m_Resize[0].empty())
+		imshow(m_strWindowName, m_Resize[0]);
+
+	return !m_Resize[0].empty();
+}
+bool cvMouseExtension::ImShow(Mat& mat, double InitScale)
+{
+	SetInitailScale(InitScale);
+	if (mat.channels() == 1)
+	{
+		cvtColor(mat, Image, COLOR_GRAY2BGR);
+	}
+	else
+	{
+		Image = mat.clone();
+	}
+	
+
+	if (Image.empty())
+		return false;
+	originalWidth = Image.cols;
+	originalHeight = Image.rows;
+
+	Size size (Image.cols * InitScale, Image.rows * InitScale);
+	resize(Image, m_Resize[0], size, 0, 0, resizeFlag);
+	if (!m_Resize[0].empty())
+		imshow (m_strWindowName, m_Resize[0]);
+
+	return !m_Resize[0].empty();
+}
+void cvMouseExtension::OnWait(Mat& curImage)
+{	
+	// 약간의 트릭 
+	imshow("Preview", curImage);
+	cv::destroyWindow("Preview");
+}
+
+void cvMouseExtension::SetInitailScale (double dScale)
+{
+	if (dScale <= 0)
+		return;
+
+	InitScale = dScale;
+	newScale = dScale;
+}
+void cvMouseExtension::InvalidateWindows()
+{
+	if (Image.empty())
+		return;
+	if (m_Resize[zoomCount].empty())
+	{
+		Size size (int (newScale * Image.cols), int (newScale * Image.rows));
+		//resize (Image, m_Resize[zoomCount], size);
+		resize(Image, m_Resize[zoomCount], size, 0, 0, resizeFlag);
+	}
+	int iW = m_Resize[0].cols - 1, iH = m_Resize[0].rows - 1;//-1: for bar size
+
+
+	Rect rectShow (Point (horizontalSliderPos, verticalSliderPos), Size (iW, iH));
+	
+	imshow (m_strWindowName, m_Resize[zoomCount](rectShow));
+}
+void cvMouseExtension::SetHorzSliderCtrlPos (int iPos)
+{
+	if (iPos > horizontalSliderRangeMax)
+		horizontalSliderPos = horizontalSliderRangeMax;
+	else if (iPos < 0)
+		horizontalSliderPos = horizontalSliderRangeMin;
+	else
+		horizontalSliderPos = iPos;
+
+}
+void cvMouseExtension::SetVertSliderCtrlPos (int iPos)
+{
+	if (iPos > verticalSliderRangeMax)
+		verticalSliderPos = verticalSliderRangeMax;
+	else if (iPos < 0)
+		verticalSliderPos = verticalSliderRangeMin;
+	else
+		verticalSliderPos = iPos;
+}
+void cvMouseExtension::OnRButtonSaveImage()
+{
+	time_t timer;
+	timer = time(NULL);
+	struct tm t;
+	localtime_s(&t, &timer);
+	cv::String temp = cv::format("%04d%02d%02d%02d%02d%d.bmp", t.tm_year + 1900, t.tm_mon + 1, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec);
+	
+	if (Image.empty())
+		return;
+	if (m_Resize[zoomCount].empty())
+	{
+		Size size(int(newScale * Image.cols), int(newScale * Image.rows));
+		resize(Image, m_Resize[zoomCount], size, 0, 0, resizeFlag);
+	}
+	int iW = m_Resize[0].cols - 1, iH = m_Resize[0].rows - 1;//-1: for bar size
+	Rect rectShow(Point(horizontalSliderPos, verticalSliderPos), Size(iW, iH));
+	OnWait(m_Resize[zoomCount](rectShow)); // 없으면 __acrt_first_block == header 오류
+	if(!m_Resize[zoomCount].empty())
+		imwrite(temp, m_Resize[zoomCount](rectShow));
+}
---
+++ cvMouseExtension.h
@@ -0,0 +1,90 @@
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+#pragma once
+class cvMouseExtension
+{
+public:
+	cvMouseExtension(String strWindowName, int iFlag = WINDOW_NORMAL);
+	/*
+	   WINDOW_NORMAL     = 0x00000000, //!< the user can resize the window (no constraint) / also use to switch a fullscreen window to a normal size.
+       WINDOW_AUTOSIZE   = 0x00000001, //!< the user cannot resize the window, the size is constrainted by the image displayed.
+       WINDOW_OPENGL     = 0x00001000, //!< window with opengl support.
+
+       WINDOW_FULLSCREEN = 1,          //!< change the window to fullscreen.
+       WINDOW_FREERATIO  = 0x00000100, //!< the image expends as much as it can (no ratio constraint).
+       WINDOW_KEEPRATIO  = 0x00000000, //!< the ratio of the image is respected.
+       WINDOW_GUI_EXPANDED=0x00000000, //!< status bar and tool bar
+       WINDOW_GUI_NORMAL = 0x00000010, //!< old fashious way
+	*/
+	~cvMouseExtension();
+
+#pragma region INIT
+	bool ImShow(Mat& mat, double InitScale = 1.0f); // default = 1
+	bool ImRead(String winname, double InitScale = 1.0f); // default = 1
+	void OnWait(Mat& curImage);
+	void SetInitailScale (double dScale);
+	bool windowExists(const string& windowName);
+#pragma endregion
+
+#pragma region FUNCTION
+	void InvalidateWindows();
+	void SetHorzSliderCtrlPos(int iPos);
+	void SetVertSliderCtrlPos(int iPos);
+	void OnRButtonSaveImage();
+
+#pragma endregion
+
+public:
+	Mat		Image;
+	vector<Mat> m_Resize;
+
+	String	m_strWindowName;
+	Point	ptLButtonDown;
+	Point	ptRButtonDown;
+
+#pragma region variable
+	int resizeFlag = 0;		
+	/*
+	INTER_NEAREST        = 0, // 리소스 작음 - 품질 낮음
+	INTER_LINEAR         = 1, // Default
+	INTER_CUBIC          = 2,
+	INTER_AREA           = 3,
+	INTER_LANCZOS4       = 4, // 리소스 큼 - 품질 높음
+	INTER_LINEAR_EXACT   = 5,
+	INTER_NEAREST_EXACT  = 6,
+	INTER_MAX            = 7,
+	WARP_FILL_OUTLIERS   = 8,
+	WARP_INVERSE_MAP     = 16
+	*/
+
+	int zoomCount;
+	int maxZoomCount;
+	int minZoomCount;
+
+	double offsetX;
+	double offsetY;
+	double InitScale;
+	double newScale;
+	double scaleRatio;
+
+	int originalWidth;
+	int originalHeight;
+
+	int horizontalSliderPos;
+	int verticalSliderPos;
+
+	int horizontalSliderPosCopy;
+	int verticalSliderPosCopy;
+
+	int horizontalSliderRangeMin;
+	int horizontalSliderRangeMax;
+	int verticalSliderRangeMin;
+	int verticalSliderRangeMax;
+
+
+#pragma endregion	
+};
+
